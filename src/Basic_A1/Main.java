3d71ad9bee887bf6625c14957b7623d25f6a72d0
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-16 09:15:55 +0200

    add and finish a1

---
+++ src/Basic_A1/Main.java
@@ -0,0 +1,13 @@
+package Basic_A1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try{
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println(e);
+        }
+    }
+}
